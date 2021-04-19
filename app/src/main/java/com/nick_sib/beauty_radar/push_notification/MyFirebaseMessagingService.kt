baf882fc8e8e3bde2467c24f092ef8d5a4f21cd8
sxespade@mail.ru <130989dimakk>
2021-04-19 20:33:06 +0300

    commit pushes

--- app/src/main/java/com/nick_sib/beauty_radar/push_notification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/nick_sib/beauty_radar/push_notification/MyFirebaseMessagingService.kt
@@ -2,16 +2,17 @@ package com.nick_sib.beauty_radar.push_notification
 
 import android.R
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.graphics.BitmapFactory
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.nick_sib.beauty_radar.ui.activity.MainActivity
 
-class MyFirebaseMessagingService: FirebaseMessagingService() {
 
+class MyFirebaseMessagingService: FirebaseMessagingService() {
 
     override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
@@ -19,11 +20,18 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
     }
 
     private fun getFireBaseMessage(contentTitle: String, contentText: String) {
+
+//        val icon = BitmapFactory.decodeResource(
+//            application.resources,
+//            R.drawable.ic_menu_add
+//        )
+
         val builder = NotificationCompat.Builder(this,"myChannel")
             .setSmallIcon(R.drawable.ic_menu_add)
             .setContentTitle(contentTitle)
             .setContentText(contentText)
             .setAutoCancel(true)
+//            .setLargeIcon(icon)
 
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -31,9 +39,9 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 
         builder.setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
 
-//        val pendingIntent =
-//            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-//        builder.setContentIntent(pendingIntent)
+        val pendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        builder.setContentIntent(pendingIntent)
 
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
