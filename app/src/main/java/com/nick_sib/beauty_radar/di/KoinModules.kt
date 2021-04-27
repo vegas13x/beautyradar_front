e7b04bd24bd2021cbdc967086dec011496e18c48
Aleksandr <mr.nevniskiy@gmail.com>
2021-04-27 08:56:37 +0300

    fix

--- app/src/main/java/com/nick_sib/beauty_radar/di/KoinModules.kt
+++ app/src/main/java/com/nick_sib/beauty_radar/di/KoinModules.kt
@@ -1,7 +1,6 @@
 package com.nick_sib.beauty_radar.di
 
 import androidx.room.Room
-import com.nick_sib.beauty_radar.model.room.HistoryDataBase
 import com.google.firebase.auth.FirebaseAuth
 import com.nick_sib.beauty_radar.model.data.state.AppState
 import com.nick_sib.beauty_radar.model.provider.auth_.AuthProviderImpl
@@ -11,20 +10,15 @@ import com.nick_sib.beauty_radar.model.provider.calendar.RemoteDBProviderCalenda
 import com.nick_sib.beauty_radar.model.provider.profile.IRemoteDBProviderProfile
 import com.nick_sib.beauty_radar.model.provider.profile.RemoteDBProviderProfile
 import com.nick_sib.beauty_radar.model.provider_new.api.ApiService
+import com.nick_sib.beauty_radar.model.provider_new.provider_db.IProviderRemoteDB
 import com.nick_sib.beauty_radar.model.provider_new.provider_db.ProviderRemoteDbImpl
 import com.nick_sib.beauty_radar.model.provider_new.retrofit.RetrofitImplementation
 import com.nick_sib.beauty_radar.model.repository.core.RemoteRepository
 import com.nick_sib.beauty_radar.model.repository.impl.RemoteRepositoryImpl
+import com.nick_sib.beauty_radar.model.room.HistoryDataBase
 import com.nick_sib.beauty_radar.model.room.IRoomSource
 import com.nick_sib.beauty_radar.model.room.RoomDataBaseImplementation
-import com.nick_sib.beauty_radar.view_model.InitialProfileSetupViewModel
-import com.nick_sib.beauty_radar.view_model.AuthViewModel
-import com.nick_sib.beauty_radar.view_model.EnterCodeViewModel
-import com.nick_sib.beauty_radar.view_model.LogoutViewModel
-import com.nick_sib.beauty_radar.view_model.MasterClientViewModel
-import com.nick_sib.beauty_radar.view_model.ProfileViewModel
-import com.nick_sib.beauty_radar.view_model.SignUpViewModel
-import com.nick_sib.beauty_radar.view_model.SignUpSecondViewModel
+import com.nick_sib.beauty_radar.view_model.*
 import com.nick_sib.beauty_radar.view_model.interactor.core.EnterCodeInteractor
 import com.nick_sib.beauty_radar.view_model.interactor.core.MasterClientInteractor
 import com.nick_sib.beauty_radar.view_model.interactor.impl.EnterCodeInteractorImpl
@@ -38,44 +32,39 @@ import org.koin.dsl.module
  * Created 08.04.2021
  */
 val appModule = module {
+
     single { FirebaseAuth.getInstance() }
-    single { RetrofitImplementation().createRetrofit() }
     single<IAuthProvider> { AuthProviderImpl(get()) }
+
     //Старый провайдер Firebase *******************
+
     single<IRemoteDBProviderProfile> { RemoteDBProviderProfile() }
     single<IRemoteDBProviderCalendar> { RemoteDBProviderCalendar() }
+
     //*********************************************
 
     //Новый провайдер для БД бэка******************
-    single { ProviderRemoteDbImpl(get()) }
-    //**********************************************
-    single { Room.databaseBuilder(get(), HistoryDataBase::class.java, "HistoryDB").build() }
-    single { get<HistoryDataBase>().historyDao() }
-    single<IRoomSource> { RoomDataBaseImplementation(get()) }
+
+    single { RetrofitImplementation().createRetrofit() }
+    single<IProviderRemoteDB> {ProviderRemoteDbImpl(get())}
+
     factory<RemoteRepository<AppState>> { RemoteRepositoryImpl(get(), get()) }
     factory<EnterCodeInteractor<AppState>> { EnterCodeInteractorImpl(get()) }
 
+    //**********************************************
+
+    single { Room.databaseBuilder(get(), HistoryDataBase::class.java, "HistoryDB").build() }
+    single { get<HistoryDataBase>().historyDao() }
+    single<IRoomSource> { RoomDataBaseImplementation(get()) }
 
 }
+
 val authFragmentModule = module {
     viewModel { AuthViewModel(get()) }
 }
-val logoutModule = module {
-    viewModel { LogoutViewModel(get()) }
-}
-val profileModule = module {
-    viewModel { ProfileViewModel(get()) }
-}
-val initialProfileModule = module {
-    viewModel { InitialProfileSetupViewModel(get()) }
-}
-val enterCodeFragmentModule = module {
 
-    viewModel { EnterCodeViewModel(get(), get(), get()) }
-}
-val masterClientFragmentModule = module {
-    factory<MasterClientInteractor<AppState>> { MasterClientInteractorImpl(get()) }
-    viewModel { MasterClientViewModel(get()) }
+val enterCodeFragmentModule = module {
+    viewModel { EnterCodeViewModel(get(), get(),get()) }
 }
 
 val signUpModule = module {
@@ -85,3 +74,21 @@ val signUpModule = module {
 val signUpSecondModule = module {
     viewModel { SignUpSecondViewModel(get()) }
 }
+
+val masterClientFragmentModule = module {
+    factory<MasterClientInteractor<AppState>> { MasterClientInteractorImpl(get()) }
+    viewModel { MasterClientViewModel(get()) }
+}
+
+val profileModule = module {
+    viewModel { ProfileViewModel(get()) }
+}
+
+val logoutModule = module {
+    viewModel { LogoutViewModel(get()) }
+}
+
+val initialProfileModule = module {
+    viewModel { InitialProfileSetupViewModel(get()) }
+}
+
