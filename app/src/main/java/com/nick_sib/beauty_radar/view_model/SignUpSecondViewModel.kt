9c2448c3e2b39662341f9fb0e2f335543c5c1b7c
Aleksandr <mr.nevniskiy@gmail.com>
2021-04-29 20:00:02 +0300

    - Проблема с регистрацией.

--- app/src/main/java/com/nick_sib/beauty_radar/view_model/SignUpSecondViewModel.kt
+++ app/src/main/java/com/nick_sib/beauty_radar/view_model/SignUpSecondViewModel.kt
@@ -1,12 +1,10 @@
 package com.nick_sib.beauty_radar.view_model
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import com.google.firebase.iid.FirebaseInstanceId
 import com.nick_sib.beauty_radar.model.data.state.AppState
 import com.nick_sib.beauty_radar.model.provider.repository.user.MasterDTO
 import com.nick_sib.beauty_radar.model.provider.repository.user.UserDTO
-import com.nick_sib.beauty_radar.view.utils.TAG_DEBAG
 import com.nick_sib.beauty_radar.view_model.base.BaseViewModel
 import com.nick_sib.beauty_radar.view_model.interactor.core.SignUpInteractor
 import kotlinx.coroutines.launch
@@ -19,7 +17,7 @@ class SignUpSecondViewModel(private val interactor: SignUpInteractor<AppState>) 
     fun createNewUser(uid: String, name: String, job: String?) {
         var master = MasterDTO(null, 100, 0)
         var token =  FirebaseInstanceId.getInstance().token
-        var user = UserDTO(null, 0, null, null,
+        var user = UserDTO(null, 0, null, token,
             master, name, null, 5, uid, token)
         uid.run {
             viewModelCoroutineScope.launch {
