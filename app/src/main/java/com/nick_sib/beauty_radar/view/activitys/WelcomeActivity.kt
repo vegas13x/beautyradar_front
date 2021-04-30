6dddead86450c6b0bc98b300901c3a4e0a6454db
nick-sib <khc1981@mail.ru>
2021-04-30 13:10:32 +0700

    font style included

--- app/src/main/java/com/nick_sib/beauty_radar/view/activitys/WelcomeActivity.kt
+++ app/src/main/java/com/nick_sib/beauty_radar/view/activitys/WelcomeActivity.kt
@@ -6,15 +6,15 @@ import android.os.Bundle
 import android.os.Handler
 import com.nick_sib.beauty_radar.R
 
-class SplashActivity : Activity() {
+class WelcomeActivity : Activity() {
 
     private val splashDuration = 2000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.screen_splash)
+        setContentView(R.layout.fragment_welcome)
         Handler(mainLooper).postDelayed({
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            val intent = Intent(this@WelcomeActivity, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             startActivity(intent)
             finish()
