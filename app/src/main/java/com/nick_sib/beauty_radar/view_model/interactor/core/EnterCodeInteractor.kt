a4fb3a561a00e712c7db5ac5dbc88bfb5b5af80d
sxespade@mail.ru <130989dimakk>
2021-05-04 20:56:09 +0300

    Получение токена апдейт-юзера в базе данных

--- app/src/main/java/com/nick_sib/beauty_radar/view_model/interactor/core/EnterCodeInteractor.kt
+++ app/src/main/java/com/nick_sib/beauty_radar/view_model/interactor/core/EnterCodeInteractor.kt
@@ -1,5 +1,11 @@
 package com.nick_sib.beauty_radar.view_model.interactor.core
 
+import com.nick_sib.beauty_radar.model.provider.repository.user.UserDTO
+
 interface EnterCodeInteractor<S> : Interactor {
     suspend fun getUserByUPNFromDB(uid: String): S
+
+    suspend fun updateUser(userDTO: UserDTO): S
+
+    suspend fun getToken(): String
 }
